9eee6d28486a918b673b84bbc177991866290280
aanchalsharma154 <61290968+aanchalsharma154@users.noreply.github.com>
2021-07-21 16:53:23 +0530

    Update userdata.sh
    Variablized the credentials for mysql and database

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -21,7 +21,7 @@ systemctl reload apache2
 a2enmod rewrite
 systemctl restart apache2
 x=$(echo "${rds_endpt}" | cut -d':' -f1)
-mysql -u drupaladmin -predhat22 -h "$x" -e "CREATE DATABASE drupal1; CREATE USER 'drupal1'@'%' IDENTIFIED BY 'drupalpass'; GRANT ALL PRIVILEGES ON drupal1.* TO 'drupal1'@'%'; FLUSH PRIVILEGES;"
+mysql -u ${mysql_user} -p${mysql_pass} -h "$x" -e "CREATE DATABASE drupal1; CREATE USER '${db_user}' IDENTIFIED BY '${db_pass}'; GRANT ALL PRIVILEGES ON drupal1.* TO '${db_user}'; FLUSH PRIVILEGES;"
 sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
 service apache2 restart
 wget https://www.drupal.org/download-latest/tar.gz -O drupal.tar.gz
